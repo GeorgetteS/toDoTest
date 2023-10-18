d471ebb726cd8c0ef43c9959bcbc6daebc01b7ab
GeorgetteS <103926946+GeorgetteS@users.noreply.github.com>
2023-10-18 19:40:31 +0300

    create onEnter

--- todoreact/src/widgets/Task-constructor/index.jsx
+++ todoreact/src/widgets/Task-constructor/index.jsx
@@ -1,19 +1,32 @@
 import cn from 'classnames';
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 import { Row } from '../../shared/ui/Row';
 import { Input } from '../../shared/ui/Input';
 
 import styles from './TaskConstructor.module.scss';
-import { CreateTask } from '../../features/create-task';
+// import { CreateTask } from '../../features/create-task';
+import { addNewtask } from '../../redux/Task/TaskSlice';
+import { Button } from '../../shared/ui/Button';
 
 export const TaskConstructor = () => {
   const [taskText, setTaskText] = useState('');
 
+  const dispath = useDispatch();
+
+  const addTask = () => {
+    dispath(addNewtask(taskText));
+    setTaskText('');
+  };
+
   return (
-    <Row className={cn(styles.root)}>
-      <Input value={taskText} onChange={(e) => setTaskText(e.target.value)}></Input>
-      <CreateTask taskValue={taskText} onCreate={() => setTaskText('')} />
+    <Row className={styles.root}>
+      <Input
+        value={taskText}
+        onEnter={addTask}
+        onChange={(e) => setTaskText(e.target.value)}></Input>
+      <Button onClick={addTask}> Добавить </Button>
     </Row>
   );
 };
